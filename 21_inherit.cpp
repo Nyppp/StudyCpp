3be37672a00b6924b9cf5f900da3f18a35ee02fa
Nyppp <wlsdnrla216@naver.com>
2022-05-31 17:55:05 +0900

    0531

--- 21_inherit.cpp
+++ 21_inherit.cpp
@@ -18,14 +18,18 @@ public:
         m_i = _a;
     }
 
-    void Output()
+    //c++에서 클래스 다형성을 위해 제공하는 virtual 키워드(가상함수)
+    //이 virtual 함수는 이 함수를 호출하는 클래스가 어떤 클래스인지 정보를 확인하게끔 해줌
+    //각 클래스들은 자신만의 고유한 가상함수 테이블을 가진다. -> 포인터로 함수를 호출할 때,
+    //자신이 가리키고 있는 객체가 가진 고유 테이블을 통해 테이블에 맞는 함수를 호출하게 됨
+    virtual void Output()
     {
         cout << "parent\n";
     }
 
 public:
-    CParent() : m_i(0) { cout << "부모 생성자\n";}
-    ~CParent() { cout << "부모 소멸자\n";}
+    CParent() : m_i(0) { }
+    ~CParent() { }
 };
 
 //CParent를 상속받는 CChild 클래스
@@ -55,6 +59,11 @@ public:
         cout << _i;    
     }
 
+    void ChildOutput()
+    {
+        cout << "only child\n";
+    }
+
 public:
     //CChild 생성자에는 이니셜라이저 맨 앞에, 부모 클래스에 대한 생성자 동작이 함축되어 있음
     // ***부모 클래스 생성자 호출은, 이니셜라이저 순서에 상관없이 가장 먼저 실행됨 + 생략해도 무관***
@@ -63,14 +72,14 @@ public:
         //생성자 이니셜라이저로는 상속 관계에 있는 변수를 사용할 수 없지만,
         //생성자 함수 내부 동작으로는 사용할 수 있음 -> 초기화는 부모 클래스에서.
         m_i = 0; 
-        cout << "자식 생성자\n";
+        
     }
     
     ~CChild()
     {
         //자식 소멸자를 먼저 호출하고 실행한 다음, 부모 소멸자를 호출함
         //부모 소멸자를 먼저 실행해버리면 안되기에
-        cout << "자식 소멸자\n";
+        
     }
 };
 
@@ -139,6 +148,53 @@ int main()
 
     //자식 클래스를 통해서 부모 함수를 명시적으로 호출가능
     //child.CParent::Output();
+
+    //상속과 포인터
+
+    //CChild 타입은 parent 클래스와 child 클래스에 대한 메모리가 존재하기에 이는 문제x
+    //CChild* pChild = &parent;
+
+    //CParent 타입은 CChild 클래스에 대한 정보가 없음.
+    //메모리 구조가 맞지 않음 + CChild 내에 구현된 기능 사용불가
+    //CParent* pParent = &child;
+
+    //자식 포인터로 부모 클래스를 가리키면 안됨
+    //부모 포인터는 자식 클래스를 받아올 수 있음(문법오류x, 캐스팅x)
+
+    //이 속성을 이용해서, 상속 관계가 많은 클래스는 모두
+    //최상위 부모 클래스의 포인터를 이용해 가리킬 수 있음
+    //하지만 그 포인터만으로는 객체가 어떤 타입인지 정확히 알수없음
+
+    CParent* pParent = nullptr;
+
+    parent.Output();
+    pParent = &parent;
+    pParent->Output();
+
+    child.Output();
+    pParent = &child;
+    pParent->Output();
+
+    //다운캐스팅
+    //부모 클래스에서 자식에만 존재하는 함수를 호출할 때, 다운 캐스팅 사용
+    //dynamic_cast 사용
+
+    //pParent에는 child에만 존재하는 기능, 멤버를 접근할 수 없음(부모 클래스 포인터기 떄문에)
+    //pParent->ChildOutput(); -> 에러 발생
+
+    //C 스타일의 강제 캐스팅 -> 실제로 동작하긴 하지만, 위험요소가 많음
+    //pParent가 child를 가리킨다는 확인이 있어야만 하며, pParent가 가리키는 객체가 바뀌면, 에러가 발생함
+    // ((CChild*)pParent)->ChildOutput();
+
+    //다이나믹 캐스트는 강제 형변환과 다르게, 실패할 경우 nullptr을 반환시켜준다. -> 안전성 보장
+    CChild* pChild = dynamic_cast<CChild*>(pParent);
+    if(pChild != nullptr)
+    {
+        pChild->ChildOutput();
+    }
+
+
+
 
     return 0;
 }
